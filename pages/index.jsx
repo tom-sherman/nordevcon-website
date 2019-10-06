e874d095429d2cae0da87907825c705d93494d98
Shaun Church <shaun@shaunchurch.com>
2019-10-06 22:24:57 +0100

    Improve speaker layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,7 +55,7 @@ function IndexRoute({
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
           Speakers
         </h1>
-        <ul className="flex flex-wrap mb-6 lg:mb-12">
+        <ul className="flex flex-wrap mb-2 lg:mb-4 ">
           {speakers
             .filter(speaker => speaker.fields.Keynote === true)
             .sort(simpleSort)
@@ -63,7 +63,7 @@ function IndexRoute({
               <Speaker key={speaker.id} speaker={speaker} schedule={schedule} />
             ))}
         </ul>
-        <ul className="flex flex-wrap">
+        <ul className="flex flex-wrap -ml-4 -mr-4">
           {speakers
             .filter(speaker => speaker.fields.Keynote !== true)
             .sort(simpleSort)
