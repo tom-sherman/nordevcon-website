08011641722fb6740471cec9c3089c7c28f708d0
Shaun Church <shaun@shaunchurch.com>
2019-10-06 15:39:45 +0100

    Adjust section padding, add number of speakers

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ function IndexRoute({
 }) {
   return (
     <Layout>
-      <section className="p-6 pt-12" id="hero">
+      <section className="p-4 md:p-8 lg:p-12 pt-12" id="hero">
         <p className="text-2xl md:text-4xl lg:text-5xl font-bold">
           NOR(DEV):CON
         </p>
@@ -24,17 +24,37 @@ function IndexRoute({
         <p className="text-md md:text-xl lg:text-2xl mb-4">
           26<sup>th</sup> &ndash; 28<sup>th</sup> February 2020
         </p>
-        <button className="bg-red-500 text-white p-3">Get tickets</button>
-      </section>
-      <section className="p-6" id="intro">
-        <h1>
+        <h1 className="w-1/2">
           <strong>nor(DEV):con is back in 2020.</strong> The eastern region's
           biggest tech conference returns in February with a fantastic line-up
           of speakers and workshops.
         </h1>
+        <a
+          href="https://www.eventbrite.co.uk/e/nordevcon-2020-tickets-65832042411"
+          className="bg-red-500 block text-center text-white p-3 font-bold mt-4 w-full max-w-xs rounded shadow-lg hover:bg-red-600"
+        >
+          Buy tickets
+        </a>
+        <div className="flex flex-row content-between text-center w-full mt-12">
+          <div className="flex flex-col w-1/3">
+            <span className="text-xl font-bold">{speakers.length}+</span>
+            <span className="text-sm text-gray-600">speakers</span>
+          </div>
+          <div className="flex flex-col w-1/3">
+            <span className="text-xl font-bold text-center">5</span>
+            <span className="text-sm text-gray-600">tracks</span>
+          </div>
+          <div className="flex flex-col w-1/3">
+            <span className="text-xl font-bold text-center">X</span>
+            <span className="text-sm text-gray-600">sessions</span>
+          </div>
+        </div>
       </section>
-      <section className="p-6" id="speakers">
-        <h1 className="text-6xl font-bold">Speakers</h1>
+
+      <section className="p-4 md:p-8 lg:p-12" id="speakers">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+          Speakers
+        </h1>
         <ul className="flex flex-wrap mb-6 lg:mb-12">
           {speakers
             .filter(speaker => speaker.fields.Keynote === true)
@@ -52,22 +72,24 @@ function IndexRoute({
             ))}
         </ul>
       </section>
-      <section className="p-6" id="schedule">
+
+      <section className="p-3" id="schedule">
         <h1 className="text-6xl font-bold">Schedule</h1>
         <ul>
           <li>talk</li>
           <li>talk</li>
         </ul>
       </section>
-      <section className="p-6" id="location">
+
+      <section className="p-3" id="location">
         <h1 className="text-6xl font-bold">Location</h1>
         <p>map and directions</p>
       </section>
-      <section className="p-6" id="nearby">
+      <section className="p-3" id="nearby">
         <h1 className="text-6xl font-bold">Useful things nearby</h1>
         <p>Train station, bus station, taxi numbers, services etc.</p>
       </section>
-      <section className="p-6" id="sponsors">
+      <section className="p-3" id="sponsors">
         <h1>Sponsors</h1>
         <p>Our wonderful sponsors</p>
       </section>
