f906216f7e2c299d16b7502a5eac732422f7e9ef
Shaun Church <shaun@shaunchurch.com>
2019-10-07 09:45:56 +0100

    Add bg gradient pattern

--- components/Speakers.jsx
+++ components/Speakers.jsx
@@ -8,27 +8,39 @@ function simpleSort(a, b) {
 
 function Speakers({ speakers }) {
   return (
-    <section className="p-4 md:p-8 lg:p-12" id="speakers">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
-        Speakers
-      </h1>
-      <ul className="flex flex-wrap mb-2 lg:mb-4 ">
-        {speakers
-          .filter(speaker => speaker.fields.Keynote === true)
-          .sort(simpleSort)
-          .map(speaker => (
-            <Speaker key={speaker.id} speaker={speaker} />
-          ))}
-      </ul>
-      <ul className="flex flex-wrap -ml-4 -mr-4">
-        {speakers
-          .filter(speaker => speaker.fields.Keynote !== true)
-          .sort(simpleSort)
-          .map(speaker => (
-            <Speaker key={speaker.id} speaker={speaker} />
-          ))}
-      </ul>
-    </section>
+    <>
+      <img
+        style={{
+          position: "absolute",
+          zIndex: -1,
+          width: "100%",
+          marginTop: "50px"
+        }}
+        src="/static/images/bg.svg"
+        alt="background"
+      />
+      <section className="p-4 md:p-8 lg:p-12" id="speakers">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+          Speakers
+        </h1>
+        <ul className="flex flex-wrap mb-2 lg:mb-4 ">
+          {speakers
+            .filter(speaker => speaker.fields.Keynote === true)
+            .sort(simpleSort)
+            .map(speaker => (
+              <Speaker key={speaker.id} speaker={speaker} />
+            ))}
+        </ul>
+        <ul className="flex flex-wrap -ml-4 -mr-4">
+          {speakers
+            .filter(speaker => speaker.fields.Keynote !== true)
+            .sort(simpleSort)
+            .map(speaker => (
+              <Speaker key={speaker.id} speaker={speaker} />
+            ))}
+        </ul>
+      </section>
+    </>
   );
 }
 
