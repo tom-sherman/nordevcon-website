c1b7fac5cfd0ebee3efef847ca746072dbf61219
Shaun Church <shaun@shaunchurch.com>
2019-10-20 14:11:39 +0100

    Configure next-offline

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withCSS = require("@zeit/next-css");
+const withOffline = require("next-offline");
 
 const nextConfig = {
   target: "serverless",
   env: {
     AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
-    MAPBOX_API_KEY: process.env.MAPBOX_API_KEY,
+    MAPBOX_API_KEY: process.env.MAPBOX_API_KEY
   },
   pageExtensions: ["jsx", "js"],
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
@@ -24,7 +25,28 @@ const nextConfig = {
     }
 
     return config;
+  },
+  generateInDevMode: false,
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
   }
 };
 
-module.exports = withCSS(nextConfig);
+module.exports = withOffline(withCSS(nextConfig));
