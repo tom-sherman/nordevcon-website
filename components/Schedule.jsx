0248d9b868ef54234adbd6923cff59dad9115cdd
Alex Scotton <alex@alexscotton.com>
2022-06-05 19:50:27 +0100

    Adds "sticky state" for Schedule wishlist using localStorage

--- components/Schedule.jsx
+++ components/Schedule.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Talks from "./Talks";
 import format from "date-fns/format";
+
+const ScheduleKey = 'nordev-schedule-wishlist__2022_v1';
 
 const Tracks = [
   "Main Auditorium (Track 1)",
@@ -23,8 +25,28 @@ const Theme = {
   }
 };
 
+// Good ol' bit of copy/pasta because my brain began to hurt
+// Stores state & retrieves it from localStorage
+function useStickyState(defaultValue, key) {
+  const [value, setValue] = useState(defaultValue);
+
+  useEffect(() => {
+    const stickyValue = window.localStorage.getItem(key);
+
+    if (stickyValue !== null) {
+      setValue(JSON.parse(stickyValue));
+    }
+  }, [key]);
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
 export default function({ schedule, speakers, isSharing }) {
-    const [wishlist, setWishlist] = useState([]);
+    const [wishlist, setWishlist] = useStickyState([], ScheduleKey);
 
     // @TODO: Ensure only 1 item per track can be saved
     const addWishlist = (add) => wishlist.includes(add)
