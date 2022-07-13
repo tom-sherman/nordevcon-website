611e91e6065be670647dd8ee074f21824683ba15
Tom Sherman <the.tomsherman@gmail.com>
2022-07-13 10:29:14 +0100

    Remove unused dependencies
    Fixes build warning related to next-offline

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withPurgeCSS = require("next-purgecss");
-const withOffline = require("next-offline");
-
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {};
-
-module.exports = withOffline(nextConfig);
