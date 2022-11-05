7de4e79ed63683a3fa122b07d0bdf447669a86ed
Alex Scotton <alex@alexscotton.com>
2022-11-05 22:55:29 +0000

    City skyline, with floaty things

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,91 @@
-export default function Hero({ children }) {
-  return (
-    <div className="bg-[#422D54] h-screen relative">
-      <div className="
-        absolute inset-0
-        mix-blend-multiply bg-no-repeat bg-cover bg-fixed
+import AcademicCapIcon from "@heroicons/react/24/outline/AcademicCapIcon";
+import BeakerIcon from "@heroicons/react/24/outline/BeakerIcon";
+import RocketLaunchIcon from "@heroicons/react/24/outline/RocketLaunchIcon";
+import CodeBracketIcon from "@heroicons/react/24/outline/CodeBracketIcon";
+import ChatBubbleLeftRightIcon from "@heroicons/react/24/outline/ChatBubbleLeftRightIcon";
+import ChartBarSquareIcon from "@heroicons/react/24/outline/ChartBarSquareIcon";
+import CircleStackIcon from "@heroicons/react/24/outline/CircleStackIcon";
+import CommandLineIcon from "@heroicons/react/24/outline/CommandLineIcon";
+import City from "./City";
+import Logo from "./Logo";
 
-        bg-[url('/hero/Hero__800_portrait.jpg')]
-        sm:bg-[url('/hero/Hero__1080.jpg')]
-        md:bg-[url('/hero/Hero__1200.jpg')]
-        xl:bg-[url('/hero/Hero.jpg')]
-      ">
+const Count = 15;
+const Icons = [
+  AcademicCapIcon,
+  BeakerIcon,
+  RocketLaunchIcon,
+  CodeBracketIcon,
+  ChatBubbleLeftRightIcon,
+  ChartBarSquareIcon,
+  CircleStackIcon,
+  CommandLineIcon,
+];
+
+function random(max, min = 0) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function Icon() {
+  let size = random(5, 2);
+  let hue = random(360);
+  let time = random(5000, 2000);
+  let delay = random(720, 30);
+
+  let start = [random(100), random(60, 40)];
+  let end = [
+    random(start[0] + 5, start[0] - 5),
+    random(start[1] - 40, start[1] - 20),
+  ];
+
+  let IconComponent = Icons[random(Icons.length - 1, 0)];
+
+  return (
+    <div
+      className="rise"
+      style={{
+        "--size": `${size}vh`,
+        "--x-start": `${start[0]}vw`,
+        "--y-start": `${start[1]}vh`,
+        "--x-end": `${end[0]}vw`,
+        "--y-end": `${end[1]}vh`,
+        "--time": `${time}ms`,
+        "--delay": `-${delay}s`,
+        "--color": `hsl(${hue}deg 65% 70% / 70%)`,
+        "--shadow": `hsl(${hue}deg 65% 50% / 50%)`,
+      }}
+    >
+      <IconComponent />
+    </div>
+  );
+}
+
+export default function Hero() {
+  return (
+    <div className="relative flex flex-col justify-between min-h-screen overflow-hidden bg-slate-900 ">
+      <div className="absolute inset-0 text-white">
+        {Array.from({ length: Count }, (_, i) => (
+          <Icon key={`icon_${i}`} />
+        ))}
       </div>
-      <div className="absolute inset-0 flex">
-        {children}
+      <div className="z-10 flex flex-col gap-8 p-4 py-16 md:p-16 md:pt-32 md:mb-16">
+        <div className="max-w-sm md:max-w-lg">
+          <Logo />
+        </div>
+
+        <div>
+          <p className="inline text-5xl leading-normal tracking-tight text-transparent bg-gradient-to-r from-cyan-400 via-sky-600 to-cyan-600 bg-clip-text font-display bg-move">
+            East Anglia&apos;s biggest tech conference
+          </p>
+        </div>
+
+        <div>
+          <p className="text-4xl font-extrabold tracking-tight text-white">
+            24rd &amp; 24th February 2023
+          </p>
+        </div>
+      </div>
+      <div className="z-10 -ml-24 text-white -mr-96 md:mx-0">
+        <City />
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -7,47 +7,16 @@ import Logo from "../components/Logo";
 export default function Home() {
   return (
     <Layout>
-      <Hero>
-        <div className="flex flex-col w-full p-4 py-8 mx-auto text-white md:justify-start md:p-8 max-w-7xl">
-          <div className="max-w-xs pr-4 mb-32">
-            <Logo />
-          </div>
-
-          <div className="flex-1 md:flex-none">
-            <h1 className="mb-8 text-5xl font-extrabold">
-                <span className="mr-2 font-extrabold text-purple-600">#</span>
-                NorDevCon23
-            </h1>
-            
-            <h2 className="mb-16 text-2xl font-bold !leading-relaxed md:text-3xl">
-              The eastern region&apos;s biggest tech conference.
-              
-              <span className="font-normal">
-                &nbsp;Two days for software developers, data scientists, startups, &amp; students.
-              </span>
-            </h2>
-
-            <h3 className="mb-4 text-3xl font-extrabold md:text-4xl">
-              {config.date}
-            </h3>
-          </div>
-
-          <div className="mt-16">
-            <a href={config.tickets} className="block px-12 py-8 text-2xl font-extrabold text-center bg-purple-700 rounded md:inline-block hover:bg-purple-500">
-              Get your ticket
-            </a>
-          </div>
-        </div>
-      </Hero>
+      <Hero />
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const [speakers, schedule, sponsors] = await Promise.all([
     airtable.getSpeakers(),
     airtable.getSchedule(),
-    airtable.getSponsors()
+    airtable.getSponsors(),
   ]);
 
   return {
@@ -56,6 +25,6 @@ export async function getStaticProps() {
       schedule: schedule,
       sponsors: sponsors,
     },
-    revalidate: 60
+    revalidate: 60,
   };
 }
